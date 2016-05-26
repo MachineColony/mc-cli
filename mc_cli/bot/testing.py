52b0a926f313341bafdfb7ce2d4c0c1929c8c782
Francis Tseng <f+accounts@frnsys.com>
2016-05-26 14:10:42 -0400

    an ngrok process is only started if necessary, closes #4

--- mc_cli/bot/testing.py
+++ mc_cli/bot/testing.py
@@ -1,26 +1,41 @@
 import os
+import time
 import requests
 import subprocess
 import BaseHTTPServer
 from multiprocessing.pool import ThreadPool
 
 request_data = None
+ngrok_tunnels_url = 'http://localhost:4040/api/tunnels'
 
 
 def _start_tunnel(port):
     """map a local port to a public url,
     to use as a webhook endpoint"""
-    devnull = open(os.devnull, 'w')
-    proc = subprocess.Popen(['ngrok', 'http', str(port)],
-                                  stdout=devnull)
-    proc.communicate()
+    try:
+        requests.get(ngrok_tunnels_url)
+
+    # only start ngrok process if the endpoint is unavailable
+    except requests.exceptions.ConnectionError:
+        devnull = open(os.devnull, 'w')
+        proc = subprocess.Popen(['ngrok', 'http', str(port)],
+                                    stdout=devnull)
+        proc.communicate()
 
 
-def start_tunnel(port=8181):
+def start_tunnel(port=8181, n_tries=5):
     pool = ThreadPool(processes=1)
     pool.apply_async(_start_tunnel, (port,))
 
-    resp = requests.get('http://localhost:4040/api/tunnels')
+    resp = None
+    while n_tries and resp is None:
+        try:
+            resp = requests.get(ngrok_tunnels_url)
+        except requests.exceptions.ConnectionError:
+            n_tries -= 1
+            time.sleep(1)
+    if resp is None:
+        raise requests.exceptions.ConnectTimeout
     tuns = resp.json()['tunnels']
 
     # grab the first, assuming it's the right one
